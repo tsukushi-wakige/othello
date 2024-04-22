1a2928a32288ecaf0467d9fce7ef4b72b88ba221
tsukushi-wakige <s1F102401157@iniad.org>
2024-04-23 05:50:10 +0900

    人生詰んだナリ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from 'react';
 import styles from './index.module.css';
 
@@ -13,35 +14,52 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const directions = [
+    [0, -1],
+    [0, 1],
+    [-1, 0],
+    [1, 0],
+    [-1, -1],
+    [1, -1],
+    [-1, 1],
+    [1, 1],
+  ];
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
     const newBoard = structuredClone(board);
-    newBoard[y][x] = turnColor;
-    setBoard(newBoard);
-    if (turnColor === 1) {
-      setTurnColor(2);
-    } else {
-      setTurnColor(1);
+    for (const direction of directions) {
+      for (let i = 0; i < 8; i++) {
+        if (board[y + direction[0]][x + direction[1]] === 3 - turnColor) {
+          newBoard[y][x] = turnColor;
+          setTurnColor(3 - turnColor);
+          setBoard(newBoard);
+        } else if (board[y + direction[0]][x + direction[1]] === undefined) {
+          break;
+        }
+      }
     }
-  };
-  return (
-    <div className={styles.container}>
-      <div className={styles.boadStyle}>
-        {board.map((row, y) =>
-          row.map((color, x) => (
-            <div className={styles.cellStyle} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
-              {color !== 0 && (
-                <div
-                  className={styles.stoneStyle}
-                  style={{ background: color === 1 ? '#000' : '#fff' }}
-                />
-              )}
-            </div>
-          )),
-        )}
+    return (
+      <div className={styles.container}>
+        <div className={styles.boadStyle}>
+          {board.map((row, y) =>
+            row.map((color, x) => (
+              <div
+                className={styles.cellStyle}
+                key={`${x}-${y}`}
+                onClick={() => clickHandler(x, y)}
+              >
+                {color !== 0 && (
+                  <div
+                    className={styles.stoneStyle}
+                    style={{ background: color === 1 ? '#000' : '#fff' }}
+                  />
+                )}
+              </div>
+            )),
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
 };
-
 export default Home;
