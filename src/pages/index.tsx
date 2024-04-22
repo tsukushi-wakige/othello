1f0b218995b379c241bd116e0209a0c5f26f12f9
tsukushi-wakige <s1F102401157@iniad.org>
2024-04-23 06:35:35 +0900

    そうやがち感謝

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from 'react';
 import styles from './index.module.css';
-
+const directions = [
+  [1, 0],
+  [1, 1],
+  [0, 1],
+  [-1, 1],
+  [-1, 0],
+  [-1, -1],
+  [0, -1],
+  [1, -1],
+];
 const Home = () => {
   const [turnColor, setTurnColor] = useState(1);
   const [board, setBoard] = useState([
@@ -14,52 +22,56 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-  const directions = [
-    [0, -1],
-    [0, 1],
-    [-1, 0],
-    [1, 0],
-    [-1, -1],
-    [1, -1],
-    [-1, 1],
-    [1, 1],
-  ];
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
+    // var checkRun = 'True';
     const newBoard = structuredClone(board);
     for (const direction of directions) {
-      for (let i = 0; i < 8; i++) {
-        if (board[y + direction[0]][x + direction[1]] === 3 - turnColor) {
-          newBoard[y][x] = turnColor;
-          setTurnColor(3 - turnColor);
-          setBoard(newBoard);
-        } else if (board[y + direction[0]][x + direction[1]] === undefined) {
-          break;
+      const memoryPosision = [];
+      // if (checkRun === 'False') {
+      //   break;
+      // }
+      if (board[y + direction[0]][x + direction[1]] === 3 - turnColor) {
+        for (let i = 1; i < 8; i++) {
+          if (board[y + direction[0] * i][x + direction[1] * i] === 3 - turnColor) {
+            memoryPosision[memoryPosision.length] = [y + direction[0] * i, x + direction[1] * i];
+            console.log([y + direction[0] * i], [x + direction[1] * i]);
+            // console.log(memoryPosision);
+            continue;
+          } else if (board[y + direction[0] * i][x + direction[1] * i] === turnColor) {
+            newBoard[y][x] = turnColor;
+            for (const posision of memoryPosision) {
+              newBoard[posision[0]][posision[1]] = turnColor;
+            }
+            setTurnColor(3 - turnColor);
+            setBoard(newBoard);
+            console.log(memoryPosision);
+            // var checkRun = 'False';
+            break;
+          } else {
+            break;
+          }
         }
       }
     }
-    return (
-      <div className={styles.container}>
-        <div className={styles.boadStyle}>
-          {board.map((row, y) =>
-            row.map((color, x) => (
-              <div
-                className={styles.cellStyle}
-                key={`${x}-${y}`}
-                onClick={() => clickHandler(x, y)}
-              >
-                {color !== 0 && (
-                  <div
-                    className={styles.stoneStyle}
-                    style={{ background: color === 1 ? '#000' : '#fff' }}
-                  />
-                )}
-              </div>
-            )),
-          )}
-        </div>
-      </div>
-    );
   };
+  return (
+    <div className={styles.container}>
+      <div className={styles.boardStyle}>
+        {board.map((row, y) =>
+          row.map((color, x) => (
+            <div className={styles.cellStyle} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
+              {color !== 0 && (
+                <div
+                  className={styles.stoneStyle}
+                  style={{ background: color === 1 ? '#000' : '#fff' }}
+                />
+              )}
+            </div>
+          )),
+        )}
+      </div>
+    </div>
+  );
 };
 export default Home;
